a2fde073a28f7a06d7d623843d4bbe6ecdca133a
zlpk2018 <zlpk2018@gmail.com>
2018-09-27 01:05:06 +0800

    Go + 小程序实现 websocket 相互收发消息。

--- main.go
+++ main.go
@@ -1,12 +1,55 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"github.com/gorilla/websocket"
+	"net/http"
+)
+
+var (
+	upgrader = websocket.Upgrader{
+		// 允许跨域
+		CheckOrigin: func(r *http.Request) bool {
+			return true
+		},
+	}
+)
 
 /*
-处理 http 请求 handler
+ * 处理 http 请求 handler
  */
 func wsHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("<h1>Hello ZLPK!</h1>"))
+	var (
+		wsConn *websocket.Conn
+		err    error
+		data   []byte
+	)
+
+	// http 升级为 websocket
+	if wsConn,err = upgrader.Upgrade(w, r, nil); err != nil {
+		fmt.Println("建立 WebSocket 连接失败!")
+		return
+	} else {
+		fmt.Println("建立 WebSocket 连接成功!")
+	}
+
+	// 循环接收消息并将接收到的消息原样发回去
+	for {
+		if _,data,err = wsConn.ReadMessage(); err != nil {
+			goto ERR
+		} else {
+			fmt.Printf("接收客户端消息：%s\n", data)
+		}
+
+		if err = wsConn.WriteMessage(websocket.TextMessage, data); err != nil {
+			goto ERR
+		}
+	}
+
+// 错误处理
+ERR:
+	fmt.Println("关闭 WebSocket 连接!")
+	wsConn.Close()
 }
 
 func main()  {
